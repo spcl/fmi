4b9781756098ad23f25a2ddebbfcf014f34ca81e
Roman Böhringer <r.boehringer@opencore.ch>
2021-10-09 10:18:28 +0200

    Refactored communicator tests to use multiple processes

--- tests/communicator.cpp
+++ tests/communicator.cpp
@@ -36,34 +36,34 @@ BOOST_AUTO_TEST_CASE(bcast) {
     constexpr int num_peers = 4;
     std::vector<SMI::Comm::Data<int>> d(num_peers);
     d[0] = 1;
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
-
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->bcast(d[tid], 0);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
-    for (int i = 0; i < num_peers; i++) {
-        BOOST_CHECK_EQUAL(d[i], 1);
-    }
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->bcast(d[peer_id], 0);
+    BOOST_CHECK_EQUAL(d[peer_id], 1);
 }
 
 BOOST_AUTO_TEST_CASE(scatter) {
     constexpr int num_peers = 4;
     SMI::Comm::Data<std::vector<int>> root_data{{1,2,3,4}};
     std::vector<SMI::Comm::Data<std::vector<int>>> d(num_peers, SMI::Comm::Data<std::vector<int>>(1));
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
-
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->scatter(root_data, d[tid], 0);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
-    for (int i = 0; i < num_peers; i++) {
-        BOOST_CHECK_EQUAL(d[i].get()[0], i + 1);
-    }
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->scatter(root_data, d[peer_id], 0);
+    BOOST_CHECK_EQUAL(d[peer_id].get()[0], peer_id + 1);
 }
 
 BOOST_AUTO_TEST_CASE(gather) {
@@ -75,16 +75,20 @@ BOOST_AUTO_TEST_CASE(gather) {
         std::vector<int> data(1, i + 1);
         d[i] = SMI::Comm::Data<std::vector<int>>(data);
     }
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
-
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->gather(d[tid], root_data, 0);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->gather(d[peer_id], root_data, 0);
     std::vector<int> expected{{1,2,3,4}};
-    BOOST_TEST(root_data.get() == expected, boost::test_tools::per_element());
+    if (peer_id == 0) {
+        BOOST_TEST(root_data.get() == expected, boost::test_tools::per_element());
+    }
 }
 
 BOOST_AUTO_TEST_CASE(reduce) {
@@ -94,16 +98,21 @@ BOOST_AUTO_TEST_CASE(reduce) {
     for (int i = 0; i < num_peers; i++) {
         data[i] = i + 1;
     }
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
     SMI::Utils::Function<int> f([] (auto a, auto b) {return a + b;}, true, true);
 
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->reduce(data[tid], res, 0, f);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
-    BOOST_CHECK_EQUAL(res, 10);
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->reduce(data[peer_id], res, 0, f);
+    if (peer_id == 0) {
+        BOOST_CHECK_EQUAL(res, 10);
+    }
 }
 
 BOOST_AUTO_TEST_CASE(reduce_vector) {
@@ -113,19 +122,24 @@ BOOST_AUTO_TEST_CASE(reduce_vector) {
     for (int i = 0; i < num_peers; i++) {
         data[i] = std::vector<int>{i + 1, 2 * (i + 1)};
     }
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
     SMI::Utils::Function<std::vector<int>> f([] (auto a, auto b) {
                                                     return std::vector<int>{a[0] + b[0], a[1] * b[1]};
                                                 },true, true);
 
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->reduce(data[tid], res, 0, f);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
-    std::vector<int> expected{1 + 2 + 3 + 4, 2 * 4 * 6 * 8};
-    BOOST_TEST(res.get() == expected, boost::test_tools::per_element());
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->reduce(data[peer_id], res, 0, f);
+    if (peer_id == 0) {
+        std::vector<int> expected{1 + 2 + 3 + 4, 2 * 4 * 6 * 8};
+        BOOST_TEST(res.get() == expected, boost::test_tools::per_element());
+    }
 }
 
 BOOST_AUTO_TEST_CASE(allreduce) {
@@ -135,18 +149,19 @@ BOOST_AUTO_TEST_CASE(allreduce) {
     for (int i = 0; i < num_peers; i++) {
         data[i] = i + 1;
     }
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
     SMI::Utils::Function<int> f([] (int a, int b) {return a + b;}, true, true);
 
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->allreduce(data[tid], res[tid], f);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
-    for (int i = 0; i < num_peers; i++) {
-        BOOST_CHECK_EQUAL(res[i], 10);
-    }
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->allreduce(data[peer_id], res[peer_id], f);
+    BOOST_CHECK_EQUAL(res[peer_id], 10);
 }
 
 BOOST_AUTO_TEST_CASE(allreduce_vector) {
@@ -157,21 +172,22 @@ BOOST_AUTO_TEST_CASE(allreduce_vector) {
         data[i] = std::vector<int>{i + 1, 2 * (i + 1), i + 1};
         res[i] = std::vector<int>(3);
     }
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
     SMI::Utils::Function<std::vector<int>> f([] (auto a, auto b) {
         return std::vector<int>{a[0] + b[0], a[1] * b[1], std::max(a[2], b[2])};
         },true, true);
 
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->allreduce(data[tid], res[tid], f);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->allreduce(data[peer_id], res[peer_id], f);
     std::vector<int> expected{1 + 2 + 3 + 4, 2 * 4 * 6 * 8, 4};
-    for (int i = 0; i < num_peers; i++) {
-        BOOST_TEST(res[i].get() == expected, boost::test_tools::per_element());
-    }
+    BOOST_TEST(res[peer_id].get() == expected, boost::test_tools::per_element());
 
 }
 
@@ -185,17 +201,21 @@ BOOST_AUTO_TEST_CASE(scan) {
     std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
     SMI::Utils::Function<int> f([] (int a, int b) {return a + b;}, true, true);
 
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->scan(data[tid], res[tid], f);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->scan(data[peer_id], res[peer_id], f);
     int prefix_sum = 0;
-    for (int i = 0; i < num_peers; i++) {
+    for (int i = 0; i <= peer_id; i++) {
         prefix_sum += i + 1;
-        BOOST_CHECK_EQUAL(res[i], prefix_sum);
     }
+    BOOST_CHECK_EQUAL(res[peer_id], prefix_sum);
 }
 
 BOOST_AUTO_TEST_CASE(scan_vector) {
@@ -206,26 +226,30 @@ BOOST_AUTO_TEST_CASE(scan_vector) {
         data[i] = std::vector<int>{i + 1, 2 * (i + 1), i + 1};
         res[i] = std::vector<int>(3);
     }
-    std::vector<std::unique_ptr<SMI::Communicator>> comms(num_peers);
     SMI::Utils::Function<std::vector<int>> f([] (auto a, auto b) {
         return std::vector<int>{a[0] + b[0], a[1] * b[1], std::max(a[2], b[2])};
         },true, true);
 
-    #pragma omp parallel num_threads(num_peers)
-    {
-        int tid = omp_get_thread_num();
-        comms[tid] = std::make_unique<SMI::Communicator>(tid, num_peers, config_path, comm_name);
-        comms[tid]->scan(data[tid], res[tid], f);
+    int peer_id = 0;
+    for (int i = 1; i < num_peers; i ++) {
+        int pid = fork();
+        if (pid == 0) {
+            peer_id = i;
+            break;
+        }
     }
+    auto ch = std::make_unique<SMI::Communicator>(peer_id, num_peers, config_path, comm_name);
+    ch->scan(data[peer_id], res[peer_id], f);
+
     int prefix_sum = 0;
     int product = 1;
-    for (int i = 0; i < num_peers; i++) {
+    for (int i = 0; i <= peer_id; i++) {
         prefix_sum += i + 1;
         product *= 2 * (i + 1);
-        BOOST_CHECK_EQUAL(res[i].get()[0], prefix_sum);
-        BOOST_CHECK_EQUAL(res[i].get()[1], product);
-        BOOST_CHECK_EQUAL(res[i].get()[2], i + 1);
     }
+    BOOST_CHECK_EQUAL(res[peer_id].get()[0], prefix_sum);
+    BOOST_CHECK_EQUAL(res[peer_id].get()[1], product);
+    BOOST_CHECK_EQUAL(res[peer_id].get()[2], peer_id + 1);
 
 }
 
