d46daab22bd68addf17338e337bd55e378ee8aa6
Syed Mujtaba <42322958+mujtaba1747@users.noreply.github.com>
2024-03-21 10:21:17 +0530

    Removed unused variable: credentialsProvider

--- src/comm/S3.cpp
+++ src/comm/S3.cpp
@@ -24,7 +24,6 @@ FMI::Comm::S3::S3(std::map<std::string, std::string> params, std::map<std::strin
     download_price = std::stod(model_params["download_price"]);
     upload_price = std::stod(model_params["upload_price"]);
 
-    auto credentialsProvider = Aws::MakeShared<Aws::Auth::EnvironmentAWSCredentialsProvider>(TAG);
     client = Aws::MakeUnique<Aws::S3::S3Client>(TAG, config);
 }
 
